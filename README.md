423244dde98757a7eec77d63043b86f04874c5fd
agapethos(Gabriel Bastos) <gapeto@proton.me>
2024-11-13 23:15:16 -0300

    Quase finalizando

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# fpeanut
+Landing page feita para divulgar o caso da eutanásia do esquilo Peanut The Squirrel.
+
+A aba "Inicio" apresenta o título e uma foto do esquilo.
+
+A aba "Causa" conta a história do caso, a partir de dois modais que são ativados ao pressionar o botão.
+
+A aba "Homenagear" mostra uma galeria de fotos de Peanut em um carrocel de imagens, com um botão escrito 
+"F" embaixo. Clicar no botão ativa uma mensagem no navegador.
--- scripts/causa.js
+++ scripts/causa.js
@@ -1,13 +1,26 @@
+// First modal elements
 const openModalButton = document.querySelector("#open-modal");
 const closeModalButton = document.querySelector("#close-modal");
 const modal = document.querySelector("#modal");
 const fade = document.querySelector("#fade");
 
-const toggleModal = () => {
+// Second modal elements
+const openModalButton2 = document.querySelector("#open-modal-2");
+const closeModalButton2 = document.querySelector("#close-modal-2");
+const modal2 = document.querySelector("#modal-2");
+const fade2 = document.querySelector("#fade-2");
+
+const toggleModal = (modal, fade) => {
   modal.classList.toggle("hide");
   fade.classList.toggle("hide");
 };
 
+// Event listeners for first modal
 [openModalButton, closeModalButton, fade].forEach((el) => {
-  el.addEventListener("click", () => toggleModal());
+  el.addEventListener("click", () => toggleModal(modal, fade));
+});
+
+// Event listeners for second modal
+[openModalButton2, closeModalButton2, fade2].forEach((el) => {
+  el.addEventListener("click", () => toggleModal(modal2, fade2));
 });
